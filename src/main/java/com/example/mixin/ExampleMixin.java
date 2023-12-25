24ad548b7850c01051e27bd9b9e8052d3c1ffdc3
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:35:21 -0300

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class CarbonMixin { // Using "Carbon" in the name
+public class ExampleMixin { // Using "Carbon" in the name
 
 @Inject(at = @At("HEAD"), method = "loadWorld")
 private void injectOptimizationsOnWorldLoad(CallbackInfo info) {
