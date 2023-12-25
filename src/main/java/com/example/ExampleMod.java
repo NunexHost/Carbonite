a0134809c1fea712e800b53286b8f65002e3bffd
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:27:54 -0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,18 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+public class CarbonMod implements ModInitializer {
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+public static final Logger LOGGER = LoggerFactory.getLogger("carbon"); // Using "carbon" as the mod ID
 
-		LOGGER.info("Hello Fabric world!");
-	}
+@Override
+public void onInitialize() {
+    LOGGER.info("Accelerating Minecraft with Carbon!");
+    // Add your mod's initialization code here
 }
