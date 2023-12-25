b2cbf9a2e2938cf92b09ab23d05042fa7ff83fa5
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:32:13 -0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -13,4 +13,6 @@ public static final Logger LOGGER = LoggerFactory.getLogger("carbon"); // Using 
 public void onInitialize() {
     LOGGER.info("Accelerating Minecraft with Carbon!");
     // Add your mod's initialization code here
+    
+   }
 }
