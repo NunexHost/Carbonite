5784a82d956aabfe19d09829744635f666a1adc9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:29:21 -0300

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -7,9 +7,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+public class CarbonMixin { // Using "Carbon" in the name
+
+@Inject(at = @At("HEAD"), method = "loadWorld")
+private void injectOptimizationsOnWorldLoad(CallbackInfo info) {
+    // Your optimization code here
+
+    // Example: Adjust chunk loading behavior
+    // ((ChunkManager) this.chunkManager).setChunkLoadDistance(4); // Adjust chunk load distance
 }
