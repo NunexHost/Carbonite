7c63d0ec1e137eb595f73266a2ba34f3ce29767d
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:35:45 -0300

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CarbonMod implements ModInitializer {
+public class ExampleMod implements ModInitializer {
 
 public static final Logger LOGGER = LoggerFactory.getLogger("carbon"); // Using "carbon" as the mod ID
 
