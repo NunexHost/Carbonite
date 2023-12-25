8af7fe0f85ad3c446393392ce9225277b48d2bcc
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-25 10:42:24 -0300

    Update ExampleModClient.java

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -3,8 +3,7 @@ package com.example;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ExampleModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
+
+@Override
+public void onInitializeClient() {
+    // Client-side optimization code here
